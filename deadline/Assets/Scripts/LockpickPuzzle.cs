c08165ed9ff67ff10663fbf6cf11c0ed96b16dec
Edwin Tandiono <tandiono98@gmail.com>
2018-03-06 16:36:00 +0700

    Added lockpick puzzle

--- deadline/Assets/Scripts/LockpickPuzzle.cs
+++ deadline/Assets/Scripts/LockpickPuzzle.cs
@@ -7,12 +7,14 @@ public class LockpickPuzzle : MonoBehaviour {
 	public float speed = 1;						// lockpick travel speed 
 	public float lockpick_treshold = 1.58f;		// lockpick movement treshold
 	public float win_treshold = 0.15f;			// lockpick win treshold
+	public Transform player;					// player position	
+	public Transform lockpick;					// the lockpick
 
-	private Transform lockpick_bar;			// the bar
-	public Transform lockpick;				// the lockpick
+	private Transform lockpick_bar;				// the bar
 	private int lockpick_direction;				// 1 = right, 0 = left
 	private bool puzzle_started;				// play status
-	private Vector3 lockpick_init;			// lockpick initial position
+	private Vector3 lockpick_init;				// lockpick initial position
+	private GameObject source_door;				// door that activate this puzzle
 
 	// Use this for initialization
 	void Start () {
@@ -22,11 +24,9 @@ public class LockpickPuzzle : MonoBehaviour {
 		//lockpick = transform.GetChild (1);
 		lockpick_direction = 0;
 		puzzle_started = false;
-		lockpick_init = new Vector3 (lockpick.position.x, 
-			lockpick.position.y, lockpick.position.z);
 
 		// play game
-		StartPuzzle ();
+		//StartPuzzle ();
 
 	}
 	
@@ -37,7 +37,7 @@ public class LockpickPuzzle : MonoBehaviour {
 			MoveLockpick ();
 
 			if (Input.GetKeyDown (KeyCode.Space)) {
-				
+				EndPuzzle ();
 			}
 
 		}
@@ -45,45 +45,46 @@ public class LockpickPuzzle : MonoBehaviour {
 	}
 
 	// start puzzle
-	void StartPuzzle () {
+	public void StartPuzzle (GameObject door) {
 
-		transform.position.Set (transform.position.x, transform.position.y, -2);
+		source_door = door;
+		transform.position = new Vector3 (player.position.x, player.position.y, -2);
 		puzzle_started = true;
+		lockpick_init = new Vector3 (lockpick.position.x, 
+			lockpick.position.y, lockpick.position.z);
 
 	}
 
 	// lockpick movement
 	void MoveLockpick() {
-		Debug.Log (lockpick.position);
 		if (lockpick_direction == 0) {
 			if (lockpick.transform.position.x >
 			    (lockpick_init.x - lockpick_treshold)) {
-				Debug.Log ("change position");
-				lockpick.position.Set(lockpick.position.x - 1, 
-					lockpick.position.y, lockpick.position.z);
+				lockpick.Translate (Vector3.left * Time.deltaTime * speed);
 			} else {
-				Debug.Log ("change direction");
 				lockpick_direction = 1;
 			}
 		} else {
 			if (lockpick.transform.position.x <
 				(lockpick_init.x + lockpick_treshold)) {
-				Debug.Log ("change position");
-				lockpick.position.Set(lockpick.position.x + 1, 
-					lockpick.position.y, lockpick.position.z);
+				lockpick.Translate (Vector3.right * Time.deltaTime * speed);
 			} else {
-				Debug.Log ("change direction");
 				lockpick_direction = 0;
 			}
 		}
-		Debug.Log (lockpick.position);
 	}
 
 	// end puzzle
 	void EndPuzzle() {
 		
 		puzzle_started = false;
-		transform.position.Set (transform.position.x, transform.position.y, 2);
+		if (lockpick.transform.position.x > (lockpick_init.x - win_treshold) &&
+		    lockpick.transform.position.x < (lockpick_init.x + win_treshold)) {
+			source_door.SetActive (false);
+		}
+
+		lockpick.position = lockpick_init;
+		transform.position = new Vector3 (player.position.x, player.position.y, 2);
 
 	}
 
--- deadline/Assets/Scripts/LockpickableDoor.cs
+++ deadline/Assets/Scripts/LockpickableDoor.cs
@@ -2,18 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.UI;
+
 public class LockpickableDoor : MonoBehaviour {
+
+	public CanvasGroup popup_message;	// message display
+	public GameObject lockpick_puzzle;	// the puzzle
+
+	private bool act_button_pressed;	// true when action button is pressed
+	private bool puzzle_started;		// puzzle play status
+	private Text popup_text;			// text displayed on message display
+
+	// const strings
+	private const string LOCKPICK = "Press E to lockpick";
 
 	// Use this for initialization
 	void Start () {
-		
+
+		// initialize variables
+		act_button_pressed = false;
+		puzzle_started = false;
+		popup_text = popup_message.GetComponentInChildren<Text> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKey (KeyCode.E)) {
+			act_button_pressed = true;
+		} else {
+			act_button_pressed = false;
+		}
+
 	}
 
+	// When touched by player 
+	void OnCollisionStay2D (Collision2D coll) {
 
+		// display popup on player contact and receive key press
+		if (coll.gameObject.tag == "Player" && !puzzle_started) {
+			popup_message.alpha = 1;
+			popup_text.text = LOCKPICK;
+
+			if (act_button_pressed) {
+				popup_message.alpha = 0;
+				puzzle_started = true;
+				lockpick_puzzle.GetComponent<LockpickPuzzle> ().StartPuzzle (gameObject);
+			}
+		}
+	
+	}
+
+	// disable popup when leaving goal
+	void OnCollisionExit2D(Collision2D coll) {
+		puzzle_started = false;
+		if (coll.gameObject.tag == "Player") {
+			popup_message.alpha = 0;
+		}
+
+	}
 
 }

fc92197d8415efa90b47b7e632bae014a9d4d9e8
Edwin Tandiono <tandiono98@gmail.com>
2018-03-26 22:17:05 +0700

    Fixed camera, deafult gameplay is at 1366 x 768

--- deadline/Assets/Scripts/CameraMovement.cs
+++ deadline/Assets/Scripts/CameraMovement.cs
@@ -5,35 +5,41 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
 
 	public Transform target;			// target to be tracked
-	public float speed = 1;				// tracking speed
-	public float distance = 10;			// camera distance to target
+	public float speed = 5;				// tracking speed
+	public float distance = 20;			// camera distance to target
 	public float zoom = 1;				// camera zoom
 
 	private Camera cam;					// camera component
-	private Vector3 vector_distance;	// vector made from distance
+	private float defZ;						// default z value
 
 	// Use this for initialization
 	void Start () {
 
 		// initialize variables
 		cam = GetComponent<Camera> ();
-		vector_distance = new Vector3 (0, 0, -distance);
 
 		// move camera to target
-		transform.position = target.position + vector_distance;
+		transform.position = target.position + new Vector3 (0, 0, -distance);
+		defZ = transform.position.z;
+
+		// manage camera size
+		float currentAspect = (float) Screen.width / (float) Screen.height;
+
+		cam.orthographicSize = (Screen.height )/ currentAspect / (100f * zoom);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		// manage camera size
-		cam.orthographicSize = (Screen.height ) / (100f * zoom);
-
 		// track target
 		if (target) {
 			transform.position = Vector3.Lerp (transform.position, 
-				target.position, speed) + vector_distance;
+				target.position, speed * Time.deltaTime);
+			transform.position = new Vector3(
+				transform.position.x,
+				transform.position.y,
+				defZ);
 		}
 
 	}
